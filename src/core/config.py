a874a2fdc0e9e93a5f36c60245f37cccaf370ea3
IslamT1 <90341747+IslamT1@users.noreply.github.com>
2023-11-16 21:42:29 +0300

    token added

--- src/core/config.py
+++ src/core/config.py
@@ -3,8 +3,16 @@ from environs import Env
 
 
 @dataclass
+class Token:
+    secret_key: str
+    algorithm: str
+    access_token_expires_in: int
+
+
+@dataclass
 class Config:
     api_key: str
+    token: Token
 
 
 def load_config() -> Config:
@@ -20,7 +28,9 @@ def load_config() -> Config:
     env.read_env()
 
     # Создаем экземпляр класса Config и наполняем его данными из переменных окружения
-    return Config(api_key=env('CURRENCY_API_KEY'))
+    return Config(api_key=env('CURRENCY_API_KEY'),
+                  token=Token(secret_key=env('SECRET_KEY'), algorithm=env('ALGORITHM'),
+                              access_token_expires_in=int(env('ACCESS_TOKEN_EXPIRE_MINUTES'))))
 
 
 config: Config = load_config()
