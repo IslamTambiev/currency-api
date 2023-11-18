aeba755c415738fd40ddce5823b1731b62e07548
IslamT1 <90341747+IslamT1@users.noreply.github.com>
2023-11-18 19:09:17 +0300

    login page added and token now in cookies

--- src/api/endpoints/users.py
+++ src/api/endpoints/users.py
@@ -1,8 +1,11 @@
 from datetime import timedelta
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Request, Response
 from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.security.api_key import APIKeyHeader
 
 from src.api.models.models import User, Token
 from src.core.security import get_current_active_user, create_access_token, authenticate_user
@@ -10,6 +13,8 @@ from src.database import fake_users_db
 from src.core.config import config
 
 router = APIRouter()
+templates = Jinja2Templates(directory="templates")
+token_key = APIKeyHeader(name="Authorization")
 
 
 @router.get('/')
@@ -17,14 +22,22 @@ def root():
     return {'message': 'Go to / and /'}
 
 
+@router.get("/login", response_class=HTMLResponse)
+async def login_for_access_token(request: Request):
+    context = {"request": request}
+    return templates.TemplateResponse("login.html", context)
+
+
 @router.post("/login", response_model=Token)
-async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+async def login_for_access_token(response: Response, form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     """
         Endpoint to authenticate a user and generate an access token.
         Args:
             form_data (OAuth2PasswordRequestForm): The form data containing the username and password.
         Returns:
             dict: A dictionary containing the access token and token type.
+            :param form_data:
+            :param response:
         """
     # Authenticate the user
     user = authenticate_user(fake_users_db, form_data.username, form_data.password)
@@ -41,8 +54,10 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
     access_token = create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
+    # Set the cookie with the access token
+    response.set_cookie("access_token", value=f"Bearer {access_token}", httponly=True)
     # Return the access token and token type
-    return {"access_token": access_token, "token_type": "bearer"}
+    return {"access_token": access_token, "token_type": "bearer"}  # Response(status_code=200,
 
 
 @router.get("/me/", response_model=User)
